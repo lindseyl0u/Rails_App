73010e7d7ee2d21ea4cecd0e7c4bf22addc219a1
Lindsey Taylor <linds.taylor90@gmail.com>
2017-03-01 16:40:28 -0500

    5.7 create comment model, defined user & products relations, added scopes for comment, created methods for sorting comments, styled views/product/show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,17 @@
 class Product < ApplicationRecord
   has_many :orders
-end
+  has_many :comments
 
-def self.search(search_term)
-  if Rails.env.development?
-    Product.where("name LIKE ?", "%#{search_term}%")
-  else
-  	Product.where("name ilike ?", "%#{search_terms}%")
-  end  	
+
+  def self.search(search_term)
+	if Rails.env.development?
+	   Product.where("name LIKE ?", "%#{search_term}%")
+	else
+	  Product.where("name ilike ?", "%#{search_terms}%")
+	end  	
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
 end

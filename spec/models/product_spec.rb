708341919b74fee256ba798b83ed9ce6bb208867
Lindsey Taylor <linds.taylor90@gmail.com>
2017-03-05 14:37:55 -0500

    6.2 created spec files for each model, wrote passing test for average_rating method, wrote a validation test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Product 
+context "when the product has comments" do
+  before do
+    @product = Product.create!(name: "Bubblegum")
+    @user = User.create!(first_name: "mr.", last_name: "tro", email: "psteelbus@gmail.com", password: "password")
+    @product.comments.create!(rating: 5, user: @user, body: "So delicious!!")
+    @product.comments.create!(rating: 3, user: @user, body: "They were just okay.")
+    @product.comments.create!(rating: 1, user: @user, body: "I wish I had more..")
+  	end
+
+  	it "returns the average rating of all comments" do
+  		expect(@product.average_rating).to eq (3)
+  end
+
+  it 'is not valid' do
+  	expect(Product.new(description: "Yum yum")).not_to be_valid
+  end
+end
+
+

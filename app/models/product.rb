251d957d7dbc48fad181fad4e154aa2bf3abf233
Lindsey Taylor <linds.taylor90@gmail.com>
2017-03-17 19:28:30 -0400

    6.12 trying to fix redis, still

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,15 +20,11 @@ class Product < ApplicationRecord
     comments.average(:rating).to_f
   end
 
-  def redis
-    $redis
-  end
-
   def views
-    redis.get("product:#{id}") #this is equivalent to 'GET product:1'
+    $redis.get("product:#{id}") #this is equivalent to 'GET product:1'
   end 
 
   def viewed!
-    redis.incr("product:#{id}") #this is equivalent to "INC product:1"
+    $redis.incr("product:#{id}") #this is equivalent to "INC product:1"
   end
 end

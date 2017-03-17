756ee8f4b34d2508435606506daef95b1ce0e542
Lindsey Taylor <linds.taylor90@gmail.com>
2017-03-17 14:41:06 -0400

    6.13 trying to fix heroku push failure

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
-  scope :rating_desc, -> { order(rating: :desc) }
   
   validates :body, presence: true
     validates :user, presence: true
     validates :product, presence: true
     validates :rating, numericality: { only_integer: true }
+    after_create_commit { CommentUpdateJob.perform_later(self, @user) }
+    scope :rating_desc, -> { order(rating: :desc) }
   self.per_page = 3
   WillPaginate.per_page = 3
 end
